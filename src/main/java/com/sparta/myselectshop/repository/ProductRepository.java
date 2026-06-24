33d5de558e94a0e59aa32113cae75afb49cd2a35
ssongcookie <ssongcookie1225@gmail.com>
2026-06-24 23:48:05 +0900

    - 상품 폴더 설계 - 폴더 생성 및 조회 구현 - 관심 상품에 폴더 추가 구현 - 폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findALlByUser(User user);
-
     Page<Product> findAllByUser(User user, Pageable pageable);
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
