c18eb21713e29f3a4f9b3b95af0cc933f6d928a7
ssongcookie <ssongcookie1225@gmail.com>
2026-06-26 16:21:58 +0900

    테스트 코드 작성 및 테스트 환경 설정 - build.gradle testImplementation 의존성 수정 - JpaConfig 분리 (@EnableJpaAuditing) - ProductService 단위 테스트 (Mockito) - ProductService 통합 테스트 (@SpringBootTest) - Controller 테스트 (MockMvc)

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // 스프링 부트에서 스케줄러가 작동하게 합니다.
-@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+//@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
 @SpringBootApplication
 public class MyselectshopApplication {
 
